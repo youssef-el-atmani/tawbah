b0d6cf90305bb25392de251089a81254f2a709b1
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-07-13 12:23:11 +0000

    Configure momahidoun-cards to get remote JSON data
    It can now use JSON data served by remote server

--- src/app/components/momahidoun-box/momahidoun-box.component.ts
+++ src/app/components/momahidoun-box/momahidoun-box.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { MomahidCardComponent } from '../momahid-card/momahid-card.component';
 import { MomahidCardService } from '../../services/momahid-card.service';
+import { Momahid } from '../../models/momahid.model';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-momahidoun-box',
@@ -9,8 +11,25 @@ import { MomahidCardService } from '../../services/momahid-card.service';
   styleUrl: './momahidoun-box.component.scss',
   providers: [MomahidCardService],
 })
-export class MomahidounBoxComponent {
+export class MomahidounBoxComponent implements OnInit {
   title = signal('المُـمَـهِّـدُون');
 
-  momahidCardService = inject(MomahidCardService);
+  alMomahidoun: Momahid[] = [];
+
+  constructor(private momahidCardService: MomahidCardService) {}
+
+  ngOnInit(): void {
+    this.momahidCardService
+      .getAlMomahidoun()
+      .pipe(
+        catchError((err) => {
+          console.error(
+            "Youssef: Can't get Al-Momahidoun list, maybe there is a server problem",
+            err
+          );
+          throw err;
+        })
+      )
+      .subscribe((data) => (this.alMomahidoun = data));
+  }
 }
