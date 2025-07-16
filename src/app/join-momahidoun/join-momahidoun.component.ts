c3ab80a68a84fc4ee916e485577b2426dfe1ba60
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-07-16 12:20:40 +0000

    Add basic input fields for joinMomahidoun form

--- src/app/join-momahidoun/join-momahidoun.component.ts
+++ src/app/join-momahidoun/join-momahidoun.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-join-momahidoun',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './join-momahidoun.component.html',
-  styleUrl: './join-momahidoun.component.scss'
+  styleUrl: './join-momahidoun.component.scss',
 })
 export class JoinMomahidounComponent {
+  // ----
+  momahidName = new FormControl();
+  momahidAge = new FormControl();
+  momahidTypeOfWorkStudy = new FormControl();
+  // ----
+  momahidAgeWhenFirstInteractWithPorn = new FormControl();
+  momahidAddictionDuration = new FormControl();
+  momahidPornDiscovery = new FormControl();
+  previousFailedAttempts = new FormControl();
+  lifeDuringAddiction = new FormControl();
+  // ----
+  lifeAfterTrueTawbah = new FormControl();
+  majorWorshipsThatHealedTheHeart = new FormControl();
+  majorSinsThatPoisonedTheHeart = new FormControl();
+  messageForMomahidBrother = new FormControl();
 
+  momahidForm: FormGroup = new FormGroup({
+    momahidName: new FormControl(),
+    momahidAge: new FormControl(),
+    momahidTypeOfWorkStudy: new FormControl(),
+    // ----
+    momahidAgeWhenFirstInteractWithPorn: new FormControl(),
+    momahidAddictionDuration: new FormControl(),
+    momahidPornDiscovery: new FormControl(),
+    previousFailedAttempts: new FormControl(),
+    lifeDuringAddiction: new FormControl(),
+    // ----
+    lifeAfterTrueTawbah: new FormControl(),
+    majorWorshipsThatHealedTheHeart: new FormControl(),
+    majorSinsThatPoisonedTheHeart: new FormControl(),
+    messageForMomahidBrother: new FormControl(),
+  });
+
+  submitMomahidRequest() {
+    const obj = this.momahidForm.value;
+    console.log(obj);
+  }
 }
