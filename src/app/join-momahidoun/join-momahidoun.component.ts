109ba27cf710336fc9a28f44ce4dceb6ad80b3df
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-07-16 14:03:06 +0000

    Post the submitted requests to the server

--- src/app/join-momahidoun/join-momahidoun.component.ts
+++ src/app/join-momahidoun/join-momahidoun.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {
   FormControl,
@@ -14,20 +15,20 @@ import {
 })
 export class JoinMomahidounComponent {
   // ----
-  momahidName = new FormControl();
-  momahidAge = new FormControl();
-  momahidTypeOfWorkStudy = new FormControl();
-  // ----
-  momahidAgeWhenFirstInteractWithPorn = new FormControl();
-  momahidAddictionDuration = new FormControl();
-  momahidPornDiscovery = new FormControl();
-  previousFailedAttempts = new FormControl();
-  lifeDuringAddiction = new FormControl();
-  // ----
-  lifeAfterTrueTawbah = new FormControl();
-  majorWorshipsThatHealedTheHeart = new FormControl();
-  majorSinsThatPoisonedTheHeart = new FormControl();
-  messageForMomahidBrother = new FormControl();
+  // momahidName = new FormControl();
+  // momahidAge = new FormControl();
+  // momahidTypeOfWorkStudy = new FormControl();
+  // // ----
+  // momahidAgeWhenFirstInteractWithPorn = new FormControl();
+  // momahidAddictionDuration = new FormControl();
+  // momahidPornDiscovery = new FormControl();
+  // previousFailedAttempts = new FormControl();
+  // lifeDuringAddiction = new FormControl();
+  // // ----
+  // lifeAfterTrueTawbah = new FormControl();
+  // majorWorshipsThatHealedTheHeart = new FormControl();
+  // majorSinsThatPoisonedTheHeart = new FormControl();
+  // messageForMomahidBrother = new FormControl();
 
   momahidForm: FormGroup = new FormGroup({
     momahidName: new FormControl(),
@@ -46,8 +47,14 @@ export class JoinMomahidounComponent {
     messageForMomahidBrother: new FormControl(),
   });
 
+  constructor(private http: HttpClient) {}
+
   submitMomahidRequest() {
     const obj = this.momahidForm.value;
+
     console.log(obj);
+    this.http
+      .post('http://localhost:3000/al-momahidoun', obj)
+      .subscribe((res: any) => alert('The requested saved successfully!'));
   }
 }
