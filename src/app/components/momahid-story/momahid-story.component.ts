b1a60a246e91626e743a8719809aa51a6de46485
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-07-24 07:56:03 +0000

    Shrink story by default and expand it when clicked

--- src/app/components/momahid-story/momahid-story.component.ts
+++ src/app/components/momahid-story/momahid-story.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-momahid-story',
   imports: [],
   templateUrl: './momahid-story.component.html',
-  styleUrl: './momahid-story.component.scss'
+  styleUrl: './momahid-story.component.scss',
 })
 export class MomahidStoryComponent {
+  @ViewChild('storyContent') storyContent!: ElementRef;
 
+  isStoryExpanded = false;
+  inlineStoryStyle = signal(`height: 560px; overflow: hidden;`);
+
+  expandStory() {
+    if (!this.isStoryExpanded) {
+      this.isStoryExpanded = true;
+      this.rend.removeClass(this.storyContent.nativeElement, 'shrink-story');
+    }
+  }
+
+  constructor(private elem: ElementRef, private rend: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.rend.addClass(this.storyContent.nativeElement, 'shrink-story');
+  }
 }
